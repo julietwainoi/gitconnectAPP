2af9f01295ced4dcc4ff6db4e2f677e97338c70a
julietwainoi <juliewainoi@gmail.com>
2024-09-24 15:15:46 +0300

    added tailwind

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header className="bg-gray-800 p-4">
+            <nav className="flex justify-end">
+                <ul className="flex space-x-4">
+                    <li><Link to="/" className="text-white hover:underline">Test</Link></li>
+                    <li><Link to="/home" className="text-white hover:underline">Home</Link></li>
+                    <li><Link to="/article" className="text-white hover:underline">Article</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // This tells Tailwind to scan these files for class names
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
